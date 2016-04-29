2330a1ae8859fe08be69fc60dd08cdd8dd7fa24e
Albert Morgan <morgaalb@os-class.engr.oregonstate.edu>
2016-04-28 22:03:31 -0700

    Conc 3 v2

--- cat_mut_ex/cme.c
+++ cat_mut_ex/cme.c
@@ -6,10 +6,15 @@
 #include <unistd.h>
 
 #define NUM_SEARCHERS	10
-#define NUM_INSERTERS	10
-#define NUM_DELETERS	10
+#define NUM_INSERTERS	3
+#define NUM_DELETERS	3
 
-#define MAX_DATA	32
+#define MAX_DATA	1000
+
+/* Turn this on to have the thread sleep after doing their operation.
+ * Useful for checking that the behavior is correct.
+ */
+#define ARTIFICIAL_SLEEP	1
 
 typedef unsigned int ticket_t;
 
@@ -22,12 +27,15 @@ struct shared_data {
 	ticket_t now_servicing;		/* Which ticket are we servicing? */
 	ticket_t ticket_num;		/* Next ticket */
 
-	int num_data;			/* Number of data in data[] */
+	int client_num;			/* For indexing clients */
+
+	int num_items;			/* Number of data in data[] */
 	int num_searchers;		/* Number of searchers searching */
 	int num_inserters;		/* Number of inserters inserting */
 	int num_deleters;		/* Number of deleters deleting */
 };
 
+/* Initializes the shared data struct */
 void shared_data_init(struct shared_data *sd)
 {
 	pthread_cond_init(&sd->cond_zero, NULL);
@@ -36,33 +44,55 @@ void shared_data_init(struct shared_data *sd)
 	sd->now_servicing = 0;
 	sd->ticket_num = 0;
 
-	sd->num_data = 0;
+	sd->client_num = 0;
+	sd->num_items = 0;
 	sd->num_searchers = 0;
 	sd->num_inserters = 0;
 	sd->num_deleters = 0;
 }
 
+/* Searcher thread */
 void *searcher(void *arg)
 {
 	struct shared_data *sd = (struct shared_data *)arg;
 	ticket_t ticket;
+	int i;
+	int value;
 
-	
+	int my_num;
+	pthread_mutex_lock(&sd->lock_data);
+	my_num = sd->client_num++;
+	pthread_mutex_unlock(&sd->lock_data);
+
 	for (;;) {
 		pthread_mutex_lock(&sd->lock_data);
 		ticket = sd->ticket_num++;
-		printf("Searcher taking ticket #%d\n", ticket);
+		printf("Searcher %d takes ticket #%d\n", my_num, ticket);
 		
 		while (sd->num_deleters != 0 || ticket != sd->now_servicing) {
 			pthread_cond_wait(&sd->cond_zero, &sd->lock_data);
 		}
+
+		printf("Now servicing ticket #%d (Searcher %d). Active threads are S: %d I: %d D: %d. List size is %d\n", sd->now_servicing, my_num,
+			sd->num_searchers, sd->num_inserters, sd->num_deleters, sd->num_items);
 		
 		sd->num_searchers++;
 		sd->now_servicing++;
+		pthread_cond_broadcast(&sd->cond_zero); // possibly wake up other searchers
 		pthread_mutex_unlock(&sd->lock_data);
 
 		// SEARCH HERE
-		printf("Searching\n");
+		printf("Searcher %d is searching.\n", my_num);
+		value = rand() % 1000;
+		for (i = 0; i < sd->num_items; i++) {
+			if (sd->data[i] == value) {
+				break;
+			}
+		}
+
+		if (ARTIFICIAL_SLEEP) {
+			sleep(rand() % 4);
+		}
 		
 		pthread_mutex_lock(&sd->lock_data);
 		sd->num_searchers--;
@@ -73,26 +103,61 @@ void *searcher(void *arg)
 	}
 }
 
+/* Inserter thread */
 void *inserter(void *arg)
 {
 	struct shared_data *sd = (struct shared_data *)arg;
 	ticket_t ticket;
+	int i;
+	int place, value;
 
+	int my_num;
+	pthread_mutex_lock(&sd->lock_data);
+	my_num = sd->client_num++;
+	pthread_mutex_unlock(&sd->lock_data);
+	
 	for (;;) {
 		pthread_mutex_lock(&sd->lock_data);
 		ticket = sd->ticket_num++;
-		printf("Inserter taking ticket #%d\n", ticket);
+		printf("Inserter %d takes ticket #%d\n", my_num, ticket);
 
 		while (sd->num_deleters != 0 || sd->num_inserters != 0 || sd->now_servicing != ticket) {
 			pthread_cond_wait(&sd->cond_zero, &sd->lock_data);
 		}
 
+		printf("Now servicing ticket #%d (Inserter %d). Active threads are S: %d I: %d D: %d. List size is %d\n", sd->now_servicing, my_num,
+			sd->num_searchers, sd->num_inserters, sd->num_deleters, sd->num_items);
+		
 		sd->num_inserters++;
 		sd->now_servicing++;
 		pthread_mutex_unlock(&sd->lock_data);
 
-		// INSERT HERE
-		printf("Inserting\n");
+		if (sd->num_items < MAX_DATA) {
+		
+			// INSERT HERE	
+			printf("Inserter %d is inserting\n", my_num);
+			value = rand() % 1000;
+			if (sd->num_items == 0) {
+				sd->data[0] = value;
+			} else {
+
+				place = rand() % sd->num_items;
+				printf("starting loop\n");
+				for(i = sd->num_items; i > place; i--) {
+					sd->data[i] = sd->data[i - 1];
+				}
+				sd->data[place] = value;
+			}
+
+			sd->num_items++;
+		} else {
+			printf("Inserter %d found list full.\n", my_num);
+		}
+
+
+		if (ARTIFICIAL_SLEEP) {
+			sleep(rand() % 5);
+		}
 		
 		pthread_mutex_lock(&sd->lock_data);
 		sd->num_inserters--;
@@ -103,26 +168,50 @@ void *inserter(void *arg)
 	}
 }
 
+/* Deleter thread */
 void *deleter(void *arg)
 {
 	struct shared_data *sd = (struct shared_data *)arg;
 	ticket_t ticket;
+	int place, i;
+
+	int my_num;
+	pthread_mutex_lock(&sd->lock_data);
+	my_num = sd->client_num++;
+	pthread_mutex_unlock(&sd->lock_data);
 
 	for (;;) {
 		pthread_mutex_lock(&sd->lock_data);
 		ticket = sd->ticket_num++;
-		printf("Deleter taking ticket #%d\n", ticket);
+		printf("Deleter %d takes ticket #%d\n", my_num, ticket);
 
-		while (sd->num_deleters != 0 || sd->num_inserters != 0 || sd->num_deleters != 0 || sd->now_servicing != ticket) {
+		while (sd->num_searchers != 0 || sd->num_inserters != 0 || sd->num_deleters != 0 || sd->now_servicing != ticket) {
 			pthread_cond_wait(&sd->cond_zero, &sd->lock_data);
 		}
 
+
+		printf("Now servicing ticket #%d (Deleter %d). Active threads are S: %d I: %d D: %d. List size is %d\n", sd->now_servicing, my_num,
+			sd->num_searchers, sd->num_inserters, sd->num_deleters, sd->num_items);
+		
 		sd->num_deleters++;
 		sd->now_servicing++;
 		pthread_mutex_unlock(&sd->lock_data);
 
-		// DELETE HERE
-		printf("Deleting\n");
+		if (sd->num_items > 0) {		
+			// DELETE HERE
+			printf("Deleter %d is deleting.\n", my_num);
+			place = rand() % sd->num_items;
+			for (i = place; i < sd->num_items; i++) {
+				sd->data[i] = sd->data[i + 1];
+			}
+			sd->num_items--;
+		} else {
+			printf("Deleter %d found list empty.\n", my_num);
+		}
+
+		if (ARTIFICIAL_SLEEP) {
+			sleep(rand() % 5);
+		}
 		
 		pthread_mutex_lock(&sd->lock_data);
 		sd->num_deleters--;
@@ -155,12 +244,6 @@ int main()
 	for(i = 0; i < NUM_DELETERS; i++)
 		pthread_create(&deleters[i], NULL, deleter, &sd);
 	
-	for(;;) {
-		sleep(1);
-		pthread_mutex_lock(&sd.lock_data);
-		printf("Now servicing ticket #%d. S:%d I:%d D:%d.\n", sd.now_servicing, sd.num_searchers, sd.num_inserters, sd.num_deleters);
-		pthread_mutex_unlock(&sd.lock_data);
-	}
 	pause();
 	
 	return 0;
